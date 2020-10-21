e0ab7f12f26cc4080783679c593d5d2f191a36f7
Alex Palomes <alejandropalomes@hotmail.com>
2020-10-21 18:19:31 +0200

    new safari adabtability

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,5 @@
-const header = document.querySelector('#header');
-window.onresize = function() {
-    console.log('test')
-    header.height = window.innerHeight;
+//Safari adaptability
+if (navigator.userAgent.match(/AppleWebKit/) && ! navigator.userAgent.match(/Chrome/)) {
+    alert('this is safari brower and only safari brower');
+    document.querySelector('.main__header__image').classList.add('safari');
 }
-window.onresize();
