1edb3ef35300429c5fb752ab66b293b74e50287e
Alex Palomes <alejandropalomes@hotmail.com>
2020-10-21 18:34:49 +0200

    restore default

--- assets/js/main.js
+++
@@ -1,4 +0,0 @@
-//Safari adaptability
-if (navigator.userAgent.match(/AppleWebKit/) && ! navigator.userAgent.match(/Chrome/)) {
-    document.querySelector('.main__header__image').classList.add('safari');
-}
