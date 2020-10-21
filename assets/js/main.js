55c467f19e254dc5165f210c16186801b20bd738
Alex Palomes <alejandropalomes@hotmail.com>
2020-10-21 18:21:08 +0200

    removed alert

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,4 @@
 //Safari adaptability
 if (navigator.userAgent.match(/AppleWebKit/) && ! navigator.userAgent.match(/Chrome/)) {
-    alert('this is safari brower and only safari brower');
     document.querySelector('.main__header__image').classList.add('safari');
 }
