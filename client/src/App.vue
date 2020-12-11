9e61359ddadfabd0ddfca20e235b6293f497e0d4
Alex Palomes <alejandropalomes@hotmail.com>
2020-12-11 14:46:39 +0100

    include footer

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,16 +1,21 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div id="app" class="main d-flex flex-column justify-content-between">
+    <div class="d-flex flex-grow-1 justify-content-center align-items-center">
+      <router-view/>
+    </div>
+    <Footer/>
   </div>
 </template>
 
 <script>
-import mainComponent from './components/mainComponent.vue'
+import mainComponent from './components/mainComponent.vue';
+import Footer from './components/Footer.vue';
 
 export default {
   name: 'App',
   components: {
-    mainComponent
+    mainComponent,
+    Footer
   }
 }
 </script>
---
+++ client/src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="footer d-flex justify-content-between align-items-center">
+        <span>Made by Alejandro Palomes</span>
+        <div>
+            Social Media
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Footer',
+        props: ['project'],
+    }
+</script>
