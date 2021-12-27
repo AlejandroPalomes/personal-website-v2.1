30fee6355ae1798c2aca663e67b7c062f652a0c6
Alejandro Palomes <alejandropalomes@hotmail.com>
2021-12-27 16:37:12 +0100

    new card component

---
+++ client/src/components/card/Card.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="card" style="width: 18rem;">
+      <!-- <img :src="project.image" class="card-img-top" alt="..."> -->
+      <img :src="image" class="card-img-top" alt="...">
+      <div class="card-body">
+        <!-- <p class="card-title">{{project.title}}</p>
+        <p class="card-text">{{project.category}}</p> -->
+        <p class="card-title">{{title}}</p>
+        <p class="card-text">{{body}}</p>
+      </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Card',
+        props: ['project', 'title', 'body', 'image'],
+    }
+</script>
